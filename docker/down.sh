0c5185ea083bf02255e092eac06ce19fe1405bc3
Adrian Roe <adrian@id3as.co.uk>
2024-07-03 10:19:39 +0000

    WIP on overrides and config

--- docker/down.sh
+++ docker/down.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 cd "${0%/*}"
 
 # We run the host networking down as it works for both host and docker nw
-docker compose -f core.yml -f host-networking.yml down -t 0
+docker compose -f core.yml -f turn.yml -f host-networking.yml  down # -t 0
--- shell.nix
+++ shell.nix
@@ -1,4 +1,3 @@
-
 let
   # try to keep this in line with Norsk
   pinnedNixHash = "fa9a51752f1b5de583ad5213eb621be071806663";
@@ -12,8 +11,8 @@ let
   nixpkgs =
     import pinnedNix {};
 
-  ffmpegForTests = (nixpkgs.ffmpeg-full.override { 
-  withGme = false; 
+  ffmpegForTests = (nixpkgs.ffmpeg-full.override {
+  withGme = false;
   withVmaf = true;
 
   # https://github.com/NixOS/nixpkgs/commit/0f0b89fc7bcea595d006f8323f40bb75c8a230af#diff-553d3a02dcec4459499ae8606548394f86a283651d828b2661b232f0c0aed5caR31
@@ -23,7 +22,7 @@ let
         substituteInPlace Makefile --replace '$(if $(STRIP), $(STRIP) -x $@)' '$(if $(STRIP), $(STRIP) -S $@)'
       '';
   });
-  
+
    });
 
   chromium = (nixpkgs.chromium); # .override { channel = "dev"; });
@@ -42,6 +41,7 @@ mkShell {
     nodePackages.vscode-langservers-extracted
     esbuild # presumably we can get this from NPM too???
     rsync
+    yq-go
    ];
 
   # shellHook = (if stdenv.isLinux then ''
