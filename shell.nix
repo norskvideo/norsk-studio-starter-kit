fc72ba094699c27a9e9e99c4cfc89b79b91837be
Rob Ashton <robashton@codeofrob.com>
2024-07-14 15:14:32 +0100

    update built-ins and replace shell with something un-pinned

--- shell.nix
+++ shell.nix
@@ -1,16 +1,37 @@
+{ pkgs ? import <nixpkgs> {} }:
 let
+
   # try to keep this in line with Norsk
-  pinnedNixHash = "57610d2f8f0937f39dbd72251e9614b1561942d8";
-  pinnedNix =
-    builtins.fetchGit {
-      name = "nixpkgs-pinned";
-      url = "https://github.com/NixOS/nixpkgs.git";
-      rev = "${pinnedNixHash}";
-    };
+  # pinnedNixHash = "fa9a51752f1b5de583ad5213eb621be071806663";
+  # pinnedNix =
+  #   builtins.fetchGit {
+  #     name = "nixpkgs-pinned";
+  #     url = "https://github.com/NixOS/nixpkgs.git";
+  #     rev = "${pinnedNixHash}";
+  #   };
 
-  nixpkgs =
-    import pinnedNix {};
+  nixpkgs = pkgs;
 
+  ffmpegForTests = nixpkgs.ffmpeg-full;
+
+  # ffmpegForTests = (nixpkgs.ffmpeg-full.override { 
+  # withGme = false; 
+  # withVmaf = true;
+
+  # # https://github.com/NixOS/nixpkgs/commit/0f0b89fc7bcea595d006f8323f40bb75c8a230af#diff-553d3a02dcec4459499ae8606548394f86a283651d828b2661b232f0c0aed5caR31
+  # x264 = nixpkgs.x264.overrideAttrs (old: {
+  #   postPatch = old.postPatch
+  #     + nixpkgs.lib.optionalString (nixpkgs.stdenv.isDarwin) ''
+  #       substituteInPlace Makefile --replace '$(if $(STRIP), $(STRIP) -x $@)' '$(if $(STRIP), $(STRIP) -S $@)'
+  #     '';
+  # });
+  
+  #  });
+
+  chromium = (nixpkgs.chromium); # .override { channel = "dev"; });
+
+  inherit (nixpkgs.stdenv.lib) optionals;
+  inherit (nixpkgs)stdenv;
 in
 
 with nixpkgs;
@@ -23,8 +44,19 @@ mkShell {
     nodePackages.vscode-langservers-extracted
     esbuild # presumably we can get this from NPM too???
     rsync
-    yq-go
    ];
 
+  # shellHook = (if stdenv.isLinux then ''
+  #   export BROWSER_FOR_TESTING=${pkgs.firefox}/bin/firefox
+  # '' else '''') +
+  # shellHook = (if stdenv.isLinux then ''
+  #   export BROWSER_FOR_TESTING=${pkgs.google-chrome}/bin/google-chrome-stable
+  # '' else '''') +
+  shellHook = (if stdenv.isLinux then ''
+    export BROWSER_FOR_TESTING=${chromium}/bin/chromium
+  '' else '''') +
+  ''
+    export FFMPEG_FULL=${ffmpegForTests}
+  '';
 }
 
