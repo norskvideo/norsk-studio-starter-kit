c4d52a08dd01d182f5852f1d45fc4823ed8e825b
Adrian Roe <roe.adrian@gmail.com>
2024-07-29 11:26:25 +0100

    WIP on Readme

--- docker/up-docker-networking-turn.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-cd "${0%/*}"
-
-docker compose -f core/norsk-media.yml -f core/norsk-studio.yml -f docker-networking/norsk-media.yml -f docker-networking/norsk-studio.yml -f turn.yml up -d
