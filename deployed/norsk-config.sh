2b3c9372ac2824ba5d680aca7a207ebaeafb2a70
Verity Scheel <monoidmusician@gmail.com>
2024-11-29 14:58:03 -0600

    bugfix

--- deployed/norsk-config.sh
+++ deployed/norsk-config.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export DEPLOY_PLATFORM="$("$(dirname "$BASH_SOURCE")/detect.sh")"
-source "$(dirname "$BASH_SOURCE")/$DEPLOY_PLATFORM/$(basename "$0")"
+source "$(dirname "$BASH_SOURCE")/$DEPLOY_PLATFORM/$(basename "$BASH_SOURCE")"
