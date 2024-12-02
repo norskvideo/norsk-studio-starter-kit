3421d5d492bc8ca73cf0fdeb5f5c2a45cf2463ff
Verity Scheel <monoidmusician@gmail.com>
2024-12-02 16:26:15 -0600

    Versions

--- deployed/norsk-containers.sh
+++ deployed/norsk-containers.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 source "$(dirname "$0")/norsk-config.sh"
+source "$(dirname "$0")/versions"
 cd "$(dirname "$0")/../docker" || exit 1
 
-export NORSK_MEDIA_IMAGE=norskvideo/norsk:v1.0.387-main
-export NORSK_STUDIO_IMAGE=norskvideo/norsk-studio:1.0.382
 export PUBLIC_URL_PREFIX=https://$DEPLOY_HOSTNAME/norsk
 
 networkDir="host-networking"
