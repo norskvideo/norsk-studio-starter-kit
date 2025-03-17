de8fe8b86995b49b0dc9f11cd8cd0eeb0b8f44e7
Verity Scheel <monoidmusician@gmail.com>
2025-03-17 17:50:22 -0500

    Updates for latest studio

--- deployed/local/norsk-config.sh
+++ deployed/local/norsk-config.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-export DEPLOY_PUBLIC_IP="127.0.0.1"
+export DEPLOY_PUBLIC_IP=${DEPLOY_PUBLIC_IP:-"127.0.0.1"}
 export DEPLOY_DOMAIN_NAME=""
-export DEPLOY_HOSTNAME="localhost"
+export DEPLOY_HOSTNAME=${DEPLOY_HOSTNAME:-"localhost"}
+
--- deployed/norsk-containers.sh
+++ deployed/norsk-containers.sh
@@ -3,7 +3,9 @@ source "$(dirname "$0")/norsk-config.sh"
 source "$(dirname "$0")/versions"
 cd "$(dirname "$0")/../docker" || exit 1
 
-export PUBLIC_URL_PREFIX=https://$DEPLOY_HOSTNAME/norsk
+export PUBLIC_URL_PREFIX=${PUBLIC_URL_PREFIX:-https://$DEPLOY_HOSTNAME/norsk}
+export STUDIO_URL_PREFIX=${STUDIO_URL_PREFIX:-/studio}
+export STUDIO_DOCS_URL=${STUDIO_DOCS_URL:-https://$DEPLOY_HOSTNAME/docs/studio/index.html}
 
 if [[ "${1:-}" = "up" || "${1:-}" = "start" ]]; then
   bash ../deployed/check-setup.sh
